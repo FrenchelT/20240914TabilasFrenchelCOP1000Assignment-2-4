fd5e5efcdb6337ae349145b0b3b88b65fb6148a2
FrenchelT <ftabilas@hawkmail.hccfl.edu>
2024-09-15 02:58:29 +0000

    20240914_TabilasFrenchel_COP1000_Assignment-2-4
    20240914_TabilasFrenchel_COP1000_Assignment-2-4

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = float(input("Enter your salary: "))
+numDependents = float(input("Enter the number of dependents: "))
 
-# calculate taxes here
+# tax rates
+stateTaxRate = 0.065 
+federalTaxRate = 0.28
+dependentDeductionRate = 0.025
+
+# calculate state tax here
+stateTax = round(salary * stateTaxRate, 2)
+federalTax = round (salary * federalTaxRate, 2)
+dependentDeduction = round (numDependents * dependentDeductionRate * salary, 2)
+totalWithholding = round (stateTax + federalTax + dependentDeduction, 2)
+
+#calculate take-home pay here
+takeHomePay = salary - totalWithholding
 
 # output statements
+print('State Tax: $' + str(stateTax))
+print('Federal Tax: $' + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
